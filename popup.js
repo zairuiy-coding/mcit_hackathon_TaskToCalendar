1c8ab1d68032b587fc4c4e6bb72a7fbb969b5a32
Zairui Yang <zairuiy@upenn.edu>
2024-01-28 00:54:11 -0500

    delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-// UI, working with popup.html
-
-document.getElementById('processData').addEventListener('click', () => {
-    const data = document.getElementById('taskData').value;
-    // Send data to background.js for processing
-    chrome.runtime.sendMessage({ action: 'processData', data: data });
-});
-
